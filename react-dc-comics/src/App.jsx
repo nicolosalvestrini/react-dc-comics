ade29e6304a3606a0b4f67e648ee640ace0f2644
nicolosalvestrini <nicolo.salvestrini@gmail.com>
2026-02-13 21:43:29 +0100

    add header page

--- react-dc-comics/src/App.jsx
+++ react-dc-comics/src/App.jsx
@@ -1,6 +1,8 @@
 import  Footer  from "./components/Footer";
 import  Main  from "./components/Main";
 import  Header  from "./components/Header";
+import { useState } from "react"
+
 
 function App() {
   const [count, setCount] = useState(0)
--- react-dc-comics/src/components/Header.jsx
+++ react-dc-comics/src/components/Header.jsx
@@ -1,8 +1,68 @@
-export default function Header () {
-    <header>
-        <div>
-            <img src="./" alt="" className="" />
-        </div>
+import logo from "../assets/img/dc-logo.png";
+export default function Header() {
+  return (
+    <header className="container">
+      <div className="d-flex align-items-center py-2 justify-content-between">
+        <img src={logo} alt="dc comics logo" className="logo-dc" />
+
+        <nav>
+          <ul className="nav">
+            <li className="nav-item">
+              <a className="nav-link text-dark" href="#">
+                CHARACTERS
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link text-primary border-bottom border-3 border-primary active"
+                href="#"
+              >
+                COMICS
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-dark" href="#">
+                MOVIES
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-dark" href="#">
+                TV
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-dark" href="#">
+                GAMES
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-dark" href="#">
+                COLLECTIBLES
+              </a>
+            </li>
+            <li className="nav-item mx-2">
+              <a className="nav-link text-dark" href="#">
+                VIDEOS
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-dark" href="#">
+                FANS
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-dark" href="#">
+                NEWS
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-dark" href="#">
+                SHOP
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
-    
+  );
 }
