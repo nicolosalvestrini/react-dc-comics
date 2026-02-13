0cf4f1356b610e5e4cbb7bbd66a39e9cb19a0f0d
nicolosalvestrini <nicolo.salvestrini@gmail.com>
2026-02-13 20:28:05 +0100

    first commit

---
+++ react-dc-comics/src/App.jsx
@@ -0,0 +1,18 @@
+import  Footer  from "./components/Footer";
+import  Main  from "./components/Main";
+import  Header  from "./components/Header";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+     
+    </>
+  )
+}
+
+export default App
---
+++ react-dc-comics/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header () {
+    <header>
+        <div>
+            <img src="./" alt="" className="" />
+        </div>
+    </header>
+    
+}
