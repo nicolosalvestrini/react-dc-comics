22a4dc63ea98036f3d0d209a9ab183b61dc593e6
nicolosalvestrini <nicolo.salvestrini@gmail.com>
2026-02-17 15:43:40 +0100

    add json comicsi and add components for comics ui

--- react-dc-comics/src/components/Main-up.jsx
+++ react-dc-comics/src/components/Main-up.jsx
@@ -1,10 +1,22 @@
-export default function MainUp () {
-    return(
-        <main className="bg-dark">
-            <div className="container py-4">
-                <h1 className="text-white">--Content goes here--</h1>
-            </div>
-        </main>
-    )
-    
+import jumbotron from "../assets/img/jumbotron.jpg";
+import FumettiCards from "../components/components-children/Fumetti.jsx";
+export default function MainUp() {
+  return (
+    <main className="bg-dark">
+        <img
+          src={jumbotron}
+          alt="jumbotron of superheros"
+          className="height-img"
+        />
+      <div className="position-relative container">
+        <h3 className="badge-title">CURRENT SERIES</h3>
+      </div>
+      <div className="container py-5">
+      <FumettiCards />
+      <button className="button d-block mx-auto">
+        LOAD MORE
+      </button>
+      </div>
+    </main>
+  );
 }
